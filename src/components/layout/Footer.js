363f6c904d6cb7aec5d154d2ec7f14b2f5a9b509
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-12-20 07:49:45 -0300

    feature: installed react icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-    <h3>Rodapé</h3>
-    </>
+    <footer>
+      <ul className="listItens">
+        <li>
+          <FaFacebook></FaFacebook>
+        </li>
+        <li>
+          <FaInstagram></FaInstagram>
+        </li>
+        <li>
+          <FaLinkedin></FaLinkedin>
+        </li>
+      </ul>
+      <p>Nosso Rodapé</p>
+    </footer>
   );
 }
 
